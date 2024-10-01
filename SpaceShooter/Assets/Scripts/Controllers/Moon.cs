a4e9056a800d1dbf8fb62616eb28408d446e9cff
Adam Prochazka <62314569+adamp02@users.noreply.github.com>
2024-10-01 00:24:09 -0400

    Task 3 - orbiting moon
    Added moon orbiting functionality

--- SpaceShooter/Assets/Scripts/Controllers/Moon.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Moon.cs
@@ -6,6 +6,11 @@ public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
+    float currentAngle = 0f;
+    public float rotationSpeed = 0.05f;
+    public float radius = 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,23 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(radius, rotationSpeed, planetTransform);
+    }
+
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        if (currentAngle > 360) { currentAngle = 0f; }
+        currentAngle += speed;
+
+        float rad = currentAngle * Mathf.Deg2Rad;
+
+        Vector3 p = new Vector3(Mathf.Cos(rad), Mathf.Sin(rad)) * radius;
+
+        transform.position = target.position + p;
+
+     
+
+     
 
     }
 }
