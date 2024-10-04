70afc363d851ec5379c6fdb13a12309cf044c5d5
Adam Prochazka <62314569+adamp02@users.noreply.github.com>
2024-10-04 03:33:19 -0400

    Test features for Week 7 submission
    added a NewPlayer class to test rigidbody physics for the final space shooter project

---
+++ SpaceShooter/Assets/Scripts/Controllers/NewPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody2D rb;
+
+    public float movementSpeed = 2f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        //rb.MovePosition(transform.position + direction * movementSpeed * Time.fixedDeltaTime);
+        rb.AddForce(direction.normalized * movementSpeed);
+    }
+}
