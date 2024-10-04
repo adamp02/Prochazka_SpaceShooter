8a78bf25c6af28adc21c2191db8f69a4ad4784f9
Adam Prochazka <62314569+adamp02@users.noreply.github.com>
2024-10-04 16:50:42 -0400

    Warp drive
    Added basic warpdrive / dodge mechanic for final space shooter (to be improved)

--- SpaceShooter/Assets/Scripts/Controllers/NewPlayer.cs
+++ SpaceShooter/Assets/Scripts/Controllers/NewPlayer.cs
@@ -10,15 +10,50 @@ public class NewPlayer : MonoBehaviour
 
     public float movementSpeed = 2f;
 
+    public bool boost = false;
+
+    public Transform hitboxDEMO;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("x")) //also check what keys are pressed -> no movement = no boost
+        {
+            boost = true;
+        }
     }
 
     void FixedUpdate()
     {
 
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-        rb.AddForce(direction.normalized * movementSpeed);
+        
+        if (boost == true && direction != Vector3.zero)
+        {
+            boost = false;
+            hitboxDEMO.gameObject.SetActive(false);
+            rb.AddForce(direction.normalized * (movementSpeed * 40f)); // also set i-frames & ghost sprite effect
+            StartCoroutine(ApplyBoost());
+
+        }
+
+        else
+        {
+            boost = false; //DOLLARAMA CODE - FIX LATER! shouldn't even get boosted at all if direction == V3.z!
+            rb.AddForce(direction.normalized * movementSpeed);
+        }
+
+    }
+
+    IEnumerator ApplyBoost()
+    {
+        yield return new WaitForSeconds(0.35f);
+        hitboxDEMO.gameObject.SetActive(true);
     }
 }
